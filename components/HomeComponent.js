40a790ff9ba4e18c453c9487bd5b5e82b6358e9e
rishabhjaincoder <rishabhjain.rdps@gmail.com>
2020-07-26 00:57:41 +0530

    React navigation 3, added featured items in home component

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,24 +1,57 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
-import { normalize } from "react-native-elements";
+import { Text, ScrollView, View } from "react-native";
+import { Card } from "react-native-elements";
+import { DISHES } from "../shared/dishes";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 // use imd to create this import {  } from 'module';
 
-class Home extends Component {
-  static navigationOptions = {
-    title: "Home",
-  };
+function RenderItem(props) {
 
-  render() {
-    return (
-      <View
-        style={{
-          alignItems: "center",
-        }}
-      >
-        <Text>Home Component</Text>
-      </View>
-    );
-  }
+    const item = props.item;
+
+    if (item != null) {
+        return (
+            <Card
+                featuredTitle={item.name}
+                featuredSubtitle={item.designation}
+                image={require('./images/uthappizza.png')}>
+                <Text
+                    style={{ margin: 10 }}>
+                    {item.description}</Text>
+            </Card>
+        );
+    }
+    else {
+        return (<View></View>);
+    }
+}
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Home',
+    };
+
+    render() {
+
+        return (
+            <ScrollView>
+                <RenderItem item={this.state.dishes.filter((dish) => dish.featured)[0]} />
+                <RenderItem item={this.state.promotions.filter((promo) => promo.featured)[0]} />
+                <RenderItem item={this.state.leaders.filter((leader) => leader.featured)[0]} />
+            </ScrollView>
+        );
+    }
 }
 
 export default Home;
